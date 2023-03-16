6dc0eab85fe39813f4d14e6352f99247dff4619d
Adam Rotman <adam@adamrotman.com>
2023-03-16 09:39:04 -0400

    Removed includes folder

--- files/keycuts/includes/foot.php
+++
@@ -1,5 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="js/html2canvas.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/script.js"></script>
